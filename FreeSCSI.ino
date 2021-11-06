0f3930a955707699f3fa99542be94720579cb1ed
Reza Fouladian <9205977+rezafouladian@users.noreply.github.com>
2021-11-06 12:46:30 -0700

    Documentation additions

--- FreeSCSI.ino
+++ FreeSCSI.ino
@@ -59,15 +59,17 @@
 SdFs SD;
 
 #if DEBUG
+// Output debug data via serial
 #define LOG(XX)     Serial.print(XX)
 #define LOGHEX(XX)  Serial.print(XX, HEX)
 #define LOGN(XX)    Serial.println(XX)
 #define LOGHEXN(XX) Serial.println(XX, HEX)
 #else
-#define LOG(XX)     //Serial.print(XX)
-#define LOGHEX(XX)  //Serial.print(XX, HEX)
-#define LOGN(XX)    //Serial.println(XX)
-#define LOGHEXN(XX) //Serial.println(XX, HEX)
+// Define debug functions as empty
+#define LOG(XX)
+#define LOGHEX(XX)
+#define LOGN(XX)
+#define LOGHEXN(XX)
 #endif
 
 #define active   1
@@ -739,6 +741,11 @@ byte onInquiryCommand(byte len)
  */
 void onRequestSenseCommand(byte len)
 {
+  /*
+  * Request Sense
+  *
+  * The Request Sense command requests that the device server transfer sense data to the application client.
+  */
   byte buf[18] = {
     0x70,   //CheckCondition
     0,      //セグメント番号
